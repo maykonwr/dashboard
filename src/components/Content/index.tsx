e0500df3aaa6764cde27898ca14125efb4a1b355
maykonwr <91572458+maykonwr@users.noreply.github.com>
2023-09-30 20:39:53 -0300

    Create index.tsx

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+
+import { Container } from './styles'
+
+interface ChildrenProps {
+    children: React.ReactNode;
+  }
+   
+  const Content: React.FC<ChildrenProps> = ({ children }) => {
+    return (
+      <Container>
+        {children}
+      </Container>
+    )
+  }
+   
+  export default Content;
