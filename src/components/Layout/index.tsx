8dd507f3ef2ea12491f0e28cb273c3cd7f12c74b
maykonwr <91572458+maykonwr@users.noreply.github.com>
2023-09-30 20:41:01 -0300

    Create index.tsx

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Grid } from './styles'
+
+import MainHeader from "../MainHeader";
+import Aside from "../Aside";
+import Content from "../Content";
+
+interface ChildrenProps {
+    children: React.ReactNode;
+  }
+
+const Layout: React.FC<ChildrenProps> = ({ children }) => {
+    return (
+        <Grid>
+            <MainHeader />
+            <Aside />
+            <Content>
+                { children }
+            </Content>
+        </Grid>
+    )
+}
+
+export default Layout
