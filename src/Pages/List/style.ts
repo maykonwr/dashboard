310b8bf4d428c21bef1529b796826219e2d5d0ae
MaykonWr <maykonsilva60@gmail.com>
2024-06-05 14:59:38 -0300

    Changed filter list

--- src/Pages/List/style.ts
+++ src/Pages/List/style.ts
@@ -20,7 +20,9 @@ export const Filters = styled.div`
         background: none;
         color: ${props => props.theme.colors.white};
         margin: 0 10px;
+
         transition: opacity .3s;
+        opacity: .4;
 
         &:hover {
             opacity: .7;
@@ -32,13 +34,16 @@ export const Filters = styled.div`
         display: block;
         width: 55px;
         margin: 0 auto;
-        border-bottom: 10px solid ${props => props.theme.colors.warning}
+        border-bottom: 10px solid ${props => props.theme.colors.success}
     }
     .tag-filter-eventual::after {
         content: '';
         display: block;
         width: 55px;
         margin: 0 auto;
-        border-bottom: 10px solid ${props => props.theme.colors.success}
+        border-bottom: 10px solid ${props => props.theme.colors.warning}
+    }
+    .tag-actived {
+        opacity: 1;
     }
 `
